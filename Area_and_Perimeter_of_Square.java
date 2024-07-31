c696340272d8ad0e65b33d7d3c4ab6b5f7ed9fc6
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2024-07-31 15:35:34 +0530

    import java.util.*; public class codemind {     public static void main(String[] agrs) {         int s;         Scanner read = new Scanner(System.in);         s = read.nextInt();         int res1 = s*s;         int res2 = 4*s;         System.out.println(res1+ " " +res2);     } }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class codemind {
+    public static void main(String[] agrs) {
+        int s;
+        Scanner read = new Scanner(System.in);
+        s = read.nextInt();
+        int res1 = s*s;
+        int res2 = 4*s;
+        System.out.println(res1+ " " +res2);
+    }
+}
