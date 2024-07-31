9b378bef4d04ef687be4d5451d8d74c4208c4180
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2024-07-31 15:29:36 +0530

    import java.util.*; public class codemind {     public static void main(String[] agrs) {         int n, m;         Scanner read = new Scanner(System.in);         n = read.nextInt();         m = read.nextInt();         int res = (n*5)+(m*7);         System.out.println(res);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class codemind {
+    public static void main(String[] agrs) {
+        int n, m;
+        Scanner read = new Scanner(System.in);
+        n = read.nextInt();
+        m = read.nextInt();
+        int res = (n*5)+(m*7);
+        System.out.println(res);
+    }
+}
