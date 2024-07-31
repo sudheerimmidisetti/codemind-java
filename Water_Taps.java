2a9c89bf5512903061c597398aa0bd3cd21f4a5a
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2024-07-31 15:10:55 +0530

    import java.util.*; public class codemind {     public static void main(String[] agrs) {         int a, b;         Scanner read = new Scanner(System.in);         a = read.nextInt();         b = read.nextInt();         int res = (a*b)/(a+b);         System.out.println(res);     } }

--- Water_Taps.java
+++ Water_Taps.java
@@ -1,11 +1,11 @@
 import java.util.*;
-public class waterTaps {
+public class codemind {
     public static void main(String[] agrs) {
-        int a, b, c;
+        int a, b;
         Scanner read = new Scanner(System.in);
         a = read.nextInt();
         b = read.nextInt();
-        c = (a*b)/(a+b);
-        System.out.println(c);
+        int res = (a*b)/(a+b);
+        System.out.println(res);
     }
 }
