c415d17781566460b168fbbce5a457976afe6191
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2024-07-31 15:12:13 +0530

    import java.util.*; public class codemind {     public static void main(String[] agrs) {         int a, b;         Scanner read = new Scanner(System.in);         a = read.nextInt();         b = read.nextInt();         double res = Math.sqrt(Math.pow(a,2)+Math.pow(b,2));         System.out.printf("%.2f",res);     } }

--- Hypotenuse.java
+++ Hypotenuse.java
@@ -1,12 +1,11 @@
 import java.util.*;
-public class waterTaps {
+public class codemind {
     public static void main(String[] agrs) {
         int a, b;
-        double c = 0;
         Scanner read = new Scanner(System.in);
         a = read.nextInt();
         b = read.nextInt();
-        c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
-        System.out.printf("%.2f",c);
+        double res = Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+        System.out.printf("%.2f",res);
     }
 }
