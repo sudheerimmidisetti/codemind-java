c6f59f36a897fae52680bd983c057f94564e328e
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2024-07-31 15:46:02 +0530

    import java.util.*; public class codemind {     public static void main(String[] agrs) {         int x, y;         Scanner read = new Scanner(System.in);         x = read.nextInt();         y = read.nextInt();         int res = (2*x)-y;         System.out.println(res);     } }

---
+++ Find_Second_Number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class codemind {
+    public static void main(String[] agrs) {
+        int x, y;
+        Scanner read = new Scanner(System.in);
+        x = read.nextInt();
+        y = read.nextInt();
+        int res = (2*x)-y;
+        System.out.println(res);
+    }
+}
