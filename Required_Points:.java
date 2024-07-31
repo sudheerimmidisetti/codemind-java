868b50484355aa2fab3bab14c5194f77c0207761
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2024-07-31 15:41:56 +0530

    import java.util.*; public class codemind {     public static void main(String[] agrs) {         int a, b;         Scanner read = new Scanner(System.in);         a = read.nextInt();         b = read.nextInt();         int res = b/a;         System.out.println(res);     } }

---
+++ Required_Points:.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class codemind {
+    public static void main(String[] agrs) {
+        int a, b;
+        Scanner read = new Scanner(System.in);
+        a = read.nextInt();
+        b = read.nextInt();
+        int res = b/a;
+        System.out.println(res);
+    }
+}
