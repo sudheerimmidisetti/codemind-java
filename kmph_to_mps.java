3b13021f4091ffb519014058b61ed04f5adc24c0
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2024-07-31 15:48:04 +0530

    import java.util.*; public class codemind {     public static void main(String[] agrs) {         int s;         Scanner read = new Scanner(System.in);         s = read.nextInt();         double res = (5.0/18)*s;         System.out.printf("%.2f",res);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class codemind {
+    public static void main(String[] agrs) {
+        int s;
+        Scanner read = new Scanner(System.in);
+        s = read.nextInt();
+        double res = (5.0/18)*s;
+        System.out.printf("%.2f",res);
+    }
+}
