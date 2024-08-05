7dc279367bca91d2d46c9ebf2474e825f6b96733
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2024-08-05 16:06:55 +0530

    import java.util.*; public class codemind {     public static void main(String[] agrs) {         Scanner read = new Scanner(System.in);         int x = read.nextInt();         int y = read.nextInt();         int z = read.nextInt();         System.out.println(((x*5)+(y*10))/z);     } }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class codemind {
+    public static void main(String[] agrs) {
+        Scanner read = new Scanner(System.in);
+        int x = read.nextInt();
+        int y = read.nextInt();
+        int z = read.nextInt();
+        System.out.println(((x*5)+(y*10))/z);
+    }
+}
