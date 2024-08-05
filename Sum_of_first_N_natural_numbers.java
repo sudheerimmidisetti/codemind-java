5302c455622efe15d6423cada0ef32ae858e6b40
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2024-08-05 15:54:35 +0530

    import java.util.*; public class codemind {     public static void main(String[] agrs) {         Scanner read = new Scanner(System.in);         int n = read.nextInt();         System.out.println(n*(n+1)/2);     } }

---
+++ Sum_of_first_N_natural_numbers.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class codemind {
+    public static void main(String[] agrs) {
+        Scanner read = new Scanner(System.in);
+        int n = read.nextInt();
+        System.out.println(n*(n+1)/2);
+    }
+}
