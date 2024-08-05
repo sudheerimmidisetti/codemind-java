2ea58fc1bb8372ac33aedb9da134bbc665cabb61
Sudheer Immidisetti <147791968+sudheerimmidisetti@users.noreply.github.com>
2024-08-05 15:48:17 +0530

    import java.util.*; public class codemind {     public static void main(String[] agrs) {         Scanner read = new Scanner(System.in);         int a = read.nextInt();         int b = read.nextInt();         System.out.println(a+b);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class codemind {
+    public static void main(String[] agrs) {
+        Scanner read = new Scanner(System.in);
+        int a = read.nextInt();
+        int b = read.nextInt();
+        System.out.println(a+b);
+    }
+}
